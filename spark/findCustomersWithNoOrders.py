fffa8a32ff14986c398c11f3bc682fdd5916a1a9
Ethan Wilson <ethanwilson1991@gmail.com>
2026-07-08 10:52:29 -0500

    update

---
+++ spark/findCustomersWithNoOrders.py
@@ -0,0 +1,11 @@
+# expected output:
+
+# | customer_id | first_name | last_name |
+# | ----------- | ---------- | --------- |
+# | 1002        | Bob        | Smith     |
+# | 1004        | David      | Wilson    |
+# | 1007        | Grace      | Moore     |
+# | 1009        | Ivy        | Anderson  |
+# | 1013        | Mia        | Martin    |
+# | 1015        | Olivia     | Garcia    |
+
